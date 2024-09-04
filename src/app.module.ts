f015314c1e5221696026f216a1a5c8fe2ad03b13
Thanh Vu <vvt4994@gmail.com>
2024-09-04 09:35:43 -0500

    19. Config .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://vvt4994:LgQtvw29ib89SH3m@cluster0.licob1z.mongodb.net/',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
