56bebc46667681c8073a42c9f86e0dfa5383bcaa
Thanh Vu <vvt4994@gmail.com>
2024-09-05 08:53:02 -0500

    25. Delete user by id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -67,7 +67,10 @@ export class UsersService {
     );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'User not found';
+    }
+    return this.userModel.deleteOne({ _id: id });
   }
 }
