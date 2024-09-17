4be2265ab2631d5d0465fed739ea110bfd484daf
Thanh Vu <vvt4994@gmail.com>
2024-09-17 04:59:32 -0500

    59.1. Query with pagination

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -6,6 +6,8 @@ import { Company, CompanyDocument } from './schemas/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from 'src/users/users.interface';
 import mongoose from 'mongoose';
+import aqp from 'api-query-params';
+import { isEmpty } from 'class-validator';
 
 // CompaniesService không biết sự tồn tại của companyModel và mongoose
 // cần phải khai báo imports ở bên module
@@ -28,9 +30,34 @@ export class CompaniesService {
     return company;
   }
 
-  findAll() {
-    const companies = this.companyModel.find({});
-    return companies;
+  async findAll(currentPage: number, limit: number, queryString: string) {
+    const { filter, sort, projection, population } = aqp(queryString);
+
+    delete filter.page;
+    delete filter.limit;
+
+    let offset = (+currentPage - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.companyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.companyModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
   }
 
   findOne(id: number) {
