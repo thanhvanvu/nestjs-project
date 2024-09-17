9976aa29b7919f4ce2827d06afed30686d3215ff
Thanh Vu <vvt4994@gmail.com>
2024-09-17 06:59:28 -0500

    63. Update User Schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,28 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
+
+class CompanyDto {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
 
 // DTO: Data Transfer Object
 export class CreateUserDto {
+  @IsNotEmpty({
+    message: 'Tên không được để trống!',
+  })
+  name: string;
+
   @IsEmail(
     {},
     {
@@ -13,9 +34,67 @@ export class CreateUserDto {
   })
   email: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({
+    message: 'Mật khẩu không được để trống!',
+  })
   password: string;
 
+  @IsNotEmpty({
+    message: 'Tuổi không được để trống!',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Giới tính không được để trống!',
+  })
+  gender: string;
+
+  address: string;
+
+  @IsNotEmpty({
+    message: 'Role không được để trống!',
+  })
+  role: string;
+
+  // validate 1 object
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => CompanyDto)
+  company: CompanyDto;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({
+    message: 'Tên không được để trống!',
+  })
   name: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'Email phải đúng định dạng!',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được để trống!',
+  })
+  email: string;
+
+  @IsNotEmpty({
+    message: 'Mật khẩu không được để trống!',
+  })
+  password: string;
+
+  @IsNotEmpty({
+    message: 'Tuổi không được để trống!',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Giới tính không được để trống!',
+  })
+  gender: string;
+
   address: string;
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -15,13 +15,43 @@ export class User {
   name: string;
 
   @Prop()
-  phone: string;
-
-  @Prop()
   age: number;
 
   @Prop()
+  gender: string;
+
+  @Prop()
   address: string;
+
+  @Prop({ type: Object })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  refreshToken: string;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
 
   @Prop()
   createdAt: Date;
