6e19b09fb869de49225bff060be755f258f365cc
Thanh Vu <vvt4994@gmail.com>
2024-09-18 06:04:03 -0500

    68. API Get Account (F5 Refresh)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,10 +8,11 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Public, ResponseMessage } from 'src/decorator/customize';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
 import { LocalAuthGuard } from './local-auth.guard';
 import { RegisterUserDto } from 'src/users/dto/create-user.dto';
 import { Response } from 'express';
+import { IUser } from 'src/users/users.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -32,5 +33,14 @@ export class AuthController {
   @Post('/register')
   handleRegister(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.registerUser(registerUserDto);
+  }
+
+  // khi mà truyền token lên header
+  // code nó sẽ chạy ở jwt.strategy passport
+  // NestJS sẽ tự động decode và lưu vô req.user
+  @ResponseMessage('Fetch account with access token')
+  @Get('/account')
+  fetchAccount(@User() user: IUser) {
+    return { user };
   }
 }
--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -7,6 +7,7 @@ import {
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
+// decorator để trả về req.user
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
