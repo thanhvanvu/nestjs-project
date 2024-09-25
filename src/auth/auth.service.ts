40ed789fb6b1931a43edd43f26ca59e06993cfc1
Thanh Vu <vvt4994@gmail.com>
2024-09-25 08:46:50 -0500

    delete console log

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -158,7 +158,6 @@ export class AuthService {
   }
 
   async logout(user: IUser, response: Response) {
-    console.log(user);
     // update refresh token to empty
     await this.usersService.updateUserToken('', user._id);
 
