bdb0146fe8ae434a508b698966920d711ea30626
Thanh Vu <vvt4994@gmail.com>
2024-09-30 18:03:01 -0500

    #99. Nestjs - Mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { PermissionsModule } from './permissions/permissions.module';
 import { RolesModule } from './roles/roles.module';
 import { DatabasesModule } from './databases/databases.module';
 import { SubscribersModule } from './subscribers/subscribers.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -54,6 +55,8 @@ import { SubscribersModule } from './subscribers/subscribers.module';
     DatabasesModule,
 
     SubscribersModule,
+
+    MailModule,
   ],
   providers: [AppService],
 })
